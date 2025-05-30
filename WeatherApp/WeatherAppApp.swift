ca7e17d009743e8c61ac6a382dda607650e5b3b4
Deser7 <deser7@icloud.com>
2025-05-30 17:16:48 +0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Наташа Спиридонова on 30.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
